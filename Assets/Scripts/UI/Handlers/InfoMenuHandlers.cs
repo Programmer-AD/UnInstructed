31ce7bdedc09855077db5b1ff5d33d6d0b31a8c6
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-01-11 22:46:11 +0200

    Made create and open folder with docs (docs must be placed manually)

--- Assets/Scripts/UI/Handlers/InfoMenuHandlers.cs
+++ Assets/Scripts/UI/Handlers/InfoMenuHandlers.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using UnityEngine;
+
 namespace Uninstructed.UI.Handlers
 {
     public class InfoMenuHandlers : MenuHandlersBase
@@ -7,6 +10,19 @@ namespace Uninstructed.UI.Handlers
         public void BackHandler()
         {
             OpenMenu(GameMenu.MainMenu);
+        }
+
+        public void InfoHandler()
+        {
+            var relativePath = Path.Combine(Application.dataPath, "Docs");
+            var fullPath = Path.GetFullPath(relativePath);
+            if (!Directory.Exists(fullPath))
+            {
+                Directory.CreateDirectory(fullPath);
+            }
+            var url = $"file:///{fullPath}";
+            Application.OpenURL(url);
+
         }
     }
 }
